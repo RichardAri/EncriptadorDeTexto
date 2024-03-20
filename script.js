70333da1e899969cc2ae4e90386c533ba34e4ee9
RichardAri <richardaripari@gmail.com>
2024-03-20 02:50:04 -0500

    Se creo el html css y js

---
+++ script.js
@@ -0,0 +1,37 @@
+function encrypt() {
+    let inputText = document.getElementById('inputText').value;
+    let outputText = '';
+    let shift = 3; // Cantidad de posiciones a desplazar
+
+    for (let i = 0; i < inputText.length; i++) {
+        let charCode = inputText.charCodeAt(i);
+        if (charCode >= 65 && charCode <= 90) {
+            outputText += String.fromCharCode(((charCode - 65 + shift) % 26) + 65);
+        } else if (charCode >= 97 && charCode <= 122) {
+            outputText += String.fromCharCode(((charCode - 97 + shift) % 26) + 97);
+        } else {
+            outputText += inputText.charAt(i);
+        }
+    }
+
+    document.getElementById('outputText').value = outputText;
+}
+
+function decrypt() {
+    let inputText = document.getElementById('outputText').value;
+    let outputText = '';
+    let shift = 3; // Cantidad de posiciones a desplazar
+
+    for (let i = 0; i < inputText.length; i++) {
+        let charCode = inputText.charCodeAt(i);
+        if (charCode >= 65 && charCode <= 90) {
+            outputText += String.fromCharCode(((charCode - 65 - shift + 26) % 26) + 65);
+        } else if (charCode >= 97 && charCode <= 122) {
+            outputText += String.fromCharCode(((charCode - 97 - shift + 26) % 26) + 97);
+        } else {
+            outputText += inputText.charAt(i);
+        }
+    }
+
+    document.getElementById('outputText').value = outputText;
+}
